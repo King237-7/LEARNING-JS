a355f2cd6961afb48fdafb7abfd5436e77c44f4b
Kamto Jeremie <kamtojeremie1@gmail.com>
2026-02-08 15:51:17 +0100

    creating the logic

--- FreeCodeCamp/Blackjack game/index.js
+++ FreeCodeCamp/Blackjack game/index.js
@@ -1,9 +1,24 @@
   let firstcard =12, secondcard = 7, sum = firstcard + secondcard;
+  let cards=[firstcard, secondcard];
   let hasblackjack = false;
   let isalive = true;
   let message = "";
 
+  let messageEl= document.getElementById("message-el");
+  let sumEl = document.getElementById("sum-el");
+  let cardEl= document.querySelector("#card-el")
+
   function startGame(){
+    renderGame();
+  }
+
+  function renderGame(){
+
+    for (let i=0; i<cards.length; i++){
+        cardEl.textContent+=" "+cards[i];
+        
+    }
+
     if(sum <= 20){
       message = "Do you want to draw a new card?";
     }
@@ -16,5 +31,15 @@
       isalive = false;
     }
     alert(message);
+    sumEl.textContent+=" "+sum;
+
+    messageEl.textContent = message;
+  }
+
+  function newCard(){
+    let card = 2;
+    sum+=card;
+    cards.push(card);
+    renderGame();
   }
 
