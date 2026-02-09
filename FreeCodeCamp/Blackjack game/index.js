56a501d92a2cd27ea9ae8bf64b9b46248fde1635
Kamto Jeremie <kamtojeremie1@gmail.com>
2026-02-09 13:52:27 +0100

    Fixing the Logic

--- FreeCodeCamp/Blackjack game/index.js
+++ FreeCodeCamp/Blackjack game/index.js
@@ -1,43 +1,40 @@
-  let firstcard =12, secondcard = 7, sum = firstcard + secondcard;
-  let cards=[firstcard, secondcard];
-  let hasblackjack = false;
-  let isalive = true;
-  let message = "";
+let firstcard =12, secondcard = 7, sum = firstcard + secondcard;
+let cards=[firstcard, secondcard];
+let hasblackjack = false;
+let isalive = true;
+let message = "";
 
-  let messageEl= document.getElementById("message-el");
-  let sumEl = document.getElementById("sum-el");
-  let cardEl= document.querySelector("#card-el")
+let messageEl= document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardEl= document.querySelector("#card-el")
 
-  function startGame(){
-    renderGame();
+function startGame(){
+  renderGame();
+}
+
+function renderGame(){
+  cardEl.textContent = "Cards:";
+  for (let i = 0; i < cards.length; i++) {
+      cardEl.textContent += " " + cards[i];
   }
 
-  function renderGame(){
-    for (let i=0; i<cards.length; i++){
-        cardEl.textContent+=" "+cards[i];
-        
-    }
-
-    if(sum <= 20){
+  if (sum <= 20) {
       message = "Do you want to draw a new card?";
-    }
-    else if(sum === 21){
+  } else if (sum === 21) {
       message = "Wohoo! You've got Blackjack!";
       hasblackjack = true;
-    }
-    else{
+  } else {
       message = "You're out of the game!";
       isalive = false;
-    }
-    sumEl.textContent+=" "+sum;
-
-    messageEl.textContent = message;
   }
+  sumEl.textContent = "Sum: " + sum; 
+  messageEl.textContent = message;
+}
 
-  function newCard(){
-    let card = 2;
-    sum+=card;
-    cards.push(card);
-    renderGame();
-  }
+function newCard() {
+  let card = 2;
+  sum += card;
+  cards.push(card);
+  renderGame(); 
+}
 
