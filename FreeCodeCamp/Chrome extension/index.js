63f68393c7a770dc7bff5b9f09dec8c1048286a0
Kamto Jeremie <kamtojeremie1@gmail.com>
2026-02-14 14:38:16 +0100

    Saving input and rendering output

--- FreeCodeCamp/Chrome extension/index.js
+++ FreeCodeCamp/Chrome extension/index.js
@@ -1,4 +1,4 @@
-let myLeads=["Test1@gmail.com", "Testemail2@gmail.com", "Lastemail@gmail.com"]
+let myLeads=[]
 const inputEL=document.getElementById("input-el")
 const inputBtn=document.getElementById("input-btn")
 const ulEl= document.getElementById("ul-el")
@@ -6,15 +6,17 @@ const ulEl= document.getElementById("ul-el")
 inputBtn.addEventListener("click", function(){
     
     myLeads.push(inputEL.value)
+    renderLeads()
     
 })
 
-let listItems=""
-for (let i=0; i<myLeads.length; i++){
-   listItems += "<li>" + myLeads[i] + "</li>"
-//    const li=document.createElement("li")
-//    li.textContent=myLeads[i]
-//    ulEl.append(li)
+function renderLeads(){
+    let listItems=""
+    for (let i=0; i<myLeads.length; i++){
+    listItems += "<li>" + myLeads[i] + "</li>"
+    //    const li=document.createElement("li")
+    //    li.textContent=myLeads[i]
+    //    ulEl.append(li)
+    }
+    ulEl.innerHTML=listItems
 }
-
-ulEl.innerHTML=listItems
