fc33f9207a4cf78b6f7da454afb9ad9551aaa9e2
Kamto Jeremie <kamtojeremie1@gmail.com>
2026-02-14 15:44:13 +0100

    Styling the links

--- FreeCodeCamp/Chrome extension/index.js
+++ FreeCodeCamp/Chrome extension/index.js
@@ -4,9 +4,9 @@ const inputBtn=document.getElementById("input-btn")
 const ulEl= document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function(){
-    let url = inputEL.value;
+    let url = inputEL.value.trim();
     if (!url.startsWith("http://") && !url.startsWith("https://")) {
-        url += "https://" 
+        url = "https://" + url 
     }
     myLeads.push(url);
     renderLeads();
@@ -16,7 +16,12 @@ inputBtn.addEventListener("click", function(){
 function renderLeads(){
     let listItems=""
     for (let i=0; i<myLeads.length; i++){
-    listItems += "<a href='" +myLeads[i]+ "'  target='_blanck' >"+"<li>" + myLeads[i] + "</li>"+"</a>"
+    listItems += `
+        <li>
+             <a href= ${myLeads[i]}  target='_blanck' > 
+             ${myLeads[i]} <a/> 
+        </li>
+        `
     //    const li=document.createElement("li")
     //    li.textContent=myLeads[i]
     //    ulEl.append(li)
